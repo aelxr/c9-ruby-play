ab019a8b46119949a08b085b2f262677d44b5031
Alex <webdvp@protonmail.com>
2018-11-14 09:13:44 +0000

    Init repo

---
+++ practice.rb
@@ -0,0 +1,33 @@
+puts 2 + 2
+puts 4 - 2
+puts 5 * 5
+puts 20.0 / 20
+
+puts Math.sqrt(16)
+puts "sd123"
+puts "hello, my name is " + "rob"
+
+name_1 = "John"
+name_2 = "Mary"
+puts name_1
+puts name_2
+
+puts "Hello my name is #{name_1}"
+
+puts 1 == 1 #true
+puts 3 == 1 #false
+
+puts 1 != 1 #false
+puts 3 != 1 #true  
+
+puts 3 > 1
+puts 3 >= 1
+puts 3 <= 1
+
+boolean1 = true
+boolean2 = false
+
+puts boolean1 && boolean2 #is false?
+puts boolean1 || boolean2 #is true?
+
+puts !boolean1 #flips values
